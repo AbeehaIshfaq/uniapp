5080c583cbe432a4561f87cd590d22d0417fad21
mahd <mahd.iftikhar@gmail.com>
2022-03-29 18:33:53 +0500

    minor fixes

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from "express";
-import StudentRouter from "./routes/student";
-import UniRouter from "./routes/university";
+import StudentRouter from "./routes/student.js";
+import UniRouter from "./routes/university.js";
 
 const app = express();
 const port = process.env.PORT;
@@ -9,6 +9,10 @@ app.use(express.json());
 
 app.use(StudentRouter);
 app.use(UniRouter);
+
+app.use("/", (req, res) => {
+    res.send("<h1>Hello World</h1>");
+});
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
