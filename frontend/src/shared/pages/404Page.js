0f928bbc2ead0cb25bfcb80bea4d74db3bc37208
mahd <mahd.iftikhar@gmail.com>
2022-04-05 19:47:01 +0500

    basic 404 page

---
+++ frontend/src/shared/pages/404Page.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Container, Segment, Header, Icon, Button } from "semantic-ui-react";
+import { Link } from "react-router-dom";
+
+const Page404 = () => {
+    return (
+        <Container textAlign="center">
+            <Segment padded="very" basic>
+                <Header as="h1" icon>
+                    <Icon name="bomb" />
+                    404
+                </Header>
+                <Header.Subheader as="h3" s>
+                    Page not Found!
+                </Header.Subheader>
+                <Button as={Link} to="/" secondary basic>
+                    Back to Home
+                </Button>
+            </Segment>
+        </Container>
+    );
+};
+
+export default Page404;
