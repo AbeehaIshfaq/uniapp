efc95d0d2592bdb679bb89cd70499842add3eaab
Antonnio <blooddreamer70@gmail.com>
2022-04-09 04:23:29 +0500

    auth0 more work

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
+import Auth0ProviderWithHistory from "./auth/auth0-provider-with-history";
+import { Auth0Provider } from "@auth0/auth0-react";
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+ReactDOM.render(
+    <Auth0Provider
+        domain="dev-8gyjrh7i.us.auth0.com"
+        clientId="t6Lxha9DKOoRi7gWypvxLVsjRheUrrUC"
+        redirectUri={window.location.origin}
+    >
+        <App />
+    </Auth0Provider>,
+    document.querySelector("#root")
+);
--- frontend/src/shared/components/landing/LandingHeader.js
+++ frontend/src/shared/components/landing/LandingHeader.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { Icon, Menu, Header, Button, Segment } from "semantic-ui-react";
 import { Link } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const LandingHeader = (props) => {
     // const { fixed } = props;
     // const fixed = false;
+    const { loginWithRedirect } = useAuth0();
+    const { logout } = useAuth0();
     return (
         <Segment basic style={{ padding: "60px 10%", border: "0" }} attached>
             <Menu secondary>
@@ -18,12 +21,39 @@ const LandingHeader = (props) => {
                 </Menu.Menu>
                 <Menu.Menu position="right">
                     <Menu.Item>
-                        <Button as={Link} to="/student/login" basic secondary>
+                        <Button
+                            onClick={() => loginWithRedirect()}
+                            // as={Link}
+                            // to="/student/login"
+                            // basic
+                            // secondary
+                        >
                             Login
                         </Button>
                     </Menu.Item>
                     <Menu.Item>
-                        <Button as={Link} to="student/signup" basic secondary>
+                        <Button
+                            onClick={() =>
+                                logout({ returnTo: window.location.origin })
+                            }
+                            // as={Link}
+                            // to="/student/login"
+                            // basic
+                            // secondary
+                        >
+                            Logout
+                        </Button>
+                    </Menu.Item>
+                    <Menu.Item>
+                        <Button
+                            onClick={() =>
+                                loginWithRedirect({ screen_hint: "signup" })
+                            }
+                            // as={Link}
+                            // to="student/signup"
+                            basic
+                            secondary
+                        >
                             Sign up
                         </Button>
                     </Menu.Item>
