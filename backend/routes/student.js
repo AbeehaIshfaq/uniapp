48b25fde2703d294ec5949e0006c4315420682e2
mahd <mahd.iftikhar@gmail.com>
2022-04-09 08:06:00 +0500

    some refactoring in the backend

--- backend/routes/student.js
+++ backend/routes/student.js
@@ -3,6 +3,8 @@ import express from "express";
 import Student from "../model/student.js";
 import Form from "../model/form.js";
 
+import { patchPersonalInfo, getPersonalInfo } from "../controller/form.js";
+
 const router = new express.Router();
 
 router.post("/createStudent", async (req, res) => {
@@ -30,48 +32,8 @@ router.get("/testForm", async (req, res) => {
     res.send({ data });
 });
 
-router.patch("/application/personalInfo", async (req, res) => {
-    console.log("PATCH /student/application/personalInfo");
-    const user = {
-        _id: "6250d009a387676f62558fc1",
-        name: "Mahd Iftikhar",
-        email: "mahd.iftikhar@gmail.com",
-        password: "password",
-    };
+router.patch("/application/personalInfo", patchPersonalInfo);
 
-    const body = req.body;
-    const name =
-        body.first + (body.middle ? ` ${body.middle} ` : " ") + body.last;
-
-    body.name = name;
-
-    try {
-        let form = await Form.findOne({ owner: user._id });
-        !form && (form = new Form({ owner: user._id }));
-
-        form.personalInfo = { ...body };
-        form.save();
-        res.send({
-            message: "Data saved successfully",
-            data: form.personalInfo,
-        });
-    } catch (err) {
-        console.log(err);
-        res.send({ error: "Something went wrong" });
-    }
-});
-
-router.get("/application/personalInfo", async (req, res) => {
-    console.log("GET /student/application/personalInfo");
-
-    const userId = "6250d009a387676f62558fc1";
-    try {
-        const form = await Form.findOne({ owner: userId });
-        res.send(form.personalInfo.toJSON());
-    } catch (err) {
-        console.log(err);
-        res.status(404).send(err);
-    }
-});
+router.get("/application/personalInfo", getPersonalInfo);
 
 export default router;
