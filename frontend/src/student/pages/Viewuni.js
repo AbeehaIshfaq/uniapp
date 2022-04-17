c76292d610f95c24c4416c701a95516e98674686
mahd <mahd.iftikhar@gmail.com>
2022-04-17 20:16:36 +0500

    fixed some bugs

--- frontend/src/student/pages/Viewuni.js
+++ frontend/src/student/pages/Viewuni.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Container } from "semantic-ui-react";
 import Navbar from "../components/navbar/Navbar";
 
-
 const Viewuni = () => {
     return (
         <>
@@ -10,60 +9,71 @@ const Viewuni = () => {
                 <Navbar />
             </header>
             <main>
-                    <h1>    </h1>
- <Container align="center" style={{ margin: "2.5rem" }}>
- <h1>  University Name  </h1>
+                <h1> </h1>
+                <Container align="center" style={{ margin: "2.5rem" }}>
+                    <h1> University Name </h1>
 
-<div align="center" class="ui steps">
-  <a class=" step">
-    <i class="clock outline icon"></i>
-    <div class="content">
-      <div class="title">Duration</div>
-      <div class="description">4 years</div>
-    </div>
-  </a>
-  <a class=" step">
-  <i class="dollar icon"></i>
-    <div class="content">
-      <div class="title">Fee</div>
-      <div class="description">800000 /year</div>
-    </div>
-  </a>
-  <a class=" step">
-  <i class="globe icon"></i>
-    <div class="content">
-      <div class="title">Location</div>
-      <div class="description">Lahore</div>
-    </div>
-  </a>
-  <a class=" step">
-  <i class="calendar alternate outline icon"></i>
-    <div class="content">
-      <div class="title">Deadline</div>
-      <div class="description">4th October</div>
-    </div>
-  </a>
+                    <div align="center" className="ui steps">
+                        <div className="step">
+                            <i className="clock outline icon"></i>
+                            <div className="content">
+                                <div className="title">Duration</div>
+                                <div className="description">4 years</div>
+                            </div>
+                        </div>
+                        <div className=" step">
+                            <i className="dollar icon"></i>
+                            <div className="content">
+                                <div className="title">Fee</div>
+                                <div className="description">800000 /year</div>
+                            </div>
+                        </div>
+                        <div className=" step">
+                            <i className="globe icon"></i>
+                            <div className="content">
+                                <div className="title">Location</div>
+                                <div className="description">Lahore</div>
+                            </div>
+                        </div>
+                        <div className=" step">
+                            <i className="calendar alternate outline icon"></i>
+                            <div className="content">
+                                <div className="title">Deadline</div>
+                                <div className="description">4th October</div>
+                            </div>
+                        </div>
+                    </div>
+                    <h3>
+                        {" "}
+                        "Sed ut perspiciatis unde omnis iste natus error sit
+                        voluptatem accusantium doloremque laudantium, totam rem
+                        aperiam, eaque ipsa quae ab illo inventore veritatis et
+                        quasi architecto beatae vitae dicta sunt explicabo. Nemo
+                        enim ipsam voluptatem quia voluptas sit aspernatur aut
+                        odit aut fugit, sed quia consequuntur magni dolores eos
+                        qui ratione voluptatem sequi nesciunt. Neque porro
+                        quisquam est, qui dolorem ipsum quia dolor sit amet,
+                        consectetur, adipisci velit, sed quia non numquam eius
+                        modi tempora incidunt ut labore et dolore magnam aliquam
+                        quaerat voluptatem. Ut enim ad minima veniam,{" "}
+                    </h3>
+                    <h2> Programes Offered </h2>
 
-</div>
-<h3>  "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam,   </h3>
-<h2>  Programes Offered  </h2>
-
-<div class="ui list">
-  <div class="item">
-    <div class="header">Computer Science</div>
-  </div>
-  <div class="item">
-    <div class="header">Maths</div>
-  </div>
-  <div class="item">
-    <div class="header"> Physics</div>
-  </div>
-  <div class="item">
-    <div class="header">Biology</div>
-  </div>
-</div>
-</Container>
-
+                    <div className="ui list">
+                        <div className="item">
+                            <div className="header">Computer Science</div>
+                        </div>
+                        <div className="item">
+                            <div className="header">Maths</div>
+                        </div>
+                        <div className="item">
+                            <div className="header"> Physics</div>
+                        </div>
+                        <div className="item">
+                            <div className="header">Biology</div>
+                        </div>
+                    </div>
+                </Container>
             </main>
         </>
     );
