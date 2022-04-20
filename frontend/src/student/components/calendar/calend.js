7ce300404d3e60b33091244ebb8d04f4c0828217
AbeehaIshfaq <92564822+AbeehaIshfaq@users.noreply.github.com>
2022-04-21 03:13:00 +0500

    able to highlight date just need to use props now

--- frontend/src/student/components/calendar/calend.js
+++ frontend/src/student/components/calendar/calend.js
@@ -18,7 +18,7 @@ import moment from "moment";
 import { Item, Button, Icon } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 import { Card, Header } from "semantic-ui-react";
-
+import './c.css';
 export default function App() 
 {
     const [dateState, setDateState] = useState(new Date());
@@ -28,6 +28,9 @@ export default function App()
   
     };
     console.log( useState(new Date()))
+    const mark = [
+        'April 1st 2022'
+    ]
     return (
         <Card fluid align="center">
             <Card.Content>
@@ -36,7 +39,21 @@ export default function App()
                 </Header>
             </Card.Content>
             <Card.Content>
-                <Calendar value={dateState} onChange={changeDate}  />
+                <Calendar 
+                
+                value={dateState} 
+                onChange={changeDate}  
+                tileClassName={({ date, view }) => {
+                    let newdate=moment(date).format("MMMM Do YYYY");
+                    console.log(newdate,"newdate");
+                    let compare=mark.find(x=>moment(x).format("MMMM Do YYYY"));
+                    console.log(compare,"compare");
+                    if(compare===newdate){
+                        console.log("MATCHED")
+                     return  'highlight'
+                    }
+                  }}
+                />
                 <br />
                 <Card.Meta>
                     Current selected date is{" "}
